8618c0dc4a976c8b724964bdfb946066844bad3e
Mario Lazzari <mario.lazzari@gmail.com>
2025-05-13 22:44:13 +0200

    refact: libs

--- components/Map/Map.tsx
+++ components/Map/Map.tsx
@@ -4,16 +4,8 @@ import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import { MapProps } from "./MapProps";
 
-// Fix Leaflet's default icon issue in Next.js
-delete (L.Icon.Default.prototype as any)._getIconUrl;
-L.Icon.Default.mergeOptions({
-  iconRetinaUrl: "/leaflet/images/marker-icon-2x.png",
-  iconUrl: "/leaflet/images/marker-icon.png",
-  shadowUrl: "/leaflet/images/marker-shadow.png",
-});
-
 const customIcon = new L.Icon({
-  iconUrl: "/pin.png", // or use an imported image
+  iconUrl: "/pin.png",
   iconSize: [32, 32], // size of the icon
   iconAnchor: [16, 32], // point of the icon which will correspond to marker's location
   popupAnchor: [0, -32], // point from which the popup should open relative to the iconAnchor
